069beced12cccbc5d7574bdecfff22fe39118276
Govind <=>
2023-03-26 23:40:56 +0530

    deployment2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { BiUserPlus } from "react-icons/bi";
+// import { BiUserPlus } from "react-icons/bi";
 import EmpForm from '../components/EmpForm';
 
 export default function Home() {
@@ -20,7 +20,8 @@ export default function Home() {
         <div className='container mx-auto flex justify-between py-5 border-b'>
         <div className='left flex gap-3'>
         <button className='flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-100 hover:border-indigo-500 hover:text-gray-800'>
-          Add Emplyee <span className='px-1'><BiUserPlus size={23}/></span>
+          {/* Add Emplyee <span className='px-1'><BiUserPlus size={23}/></span> */}
+          Add Emplyee
         </button>
         </div>      
         </div>
